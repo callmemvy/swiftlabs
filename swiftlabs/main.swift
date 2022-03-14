904fc76583f00104f2c82a238691a98960d2e0ea
mvgvy <omvgvy@gmail.com>
2022-03-14 15:43:08 +0600

    task3

--- swiftlabs/main.swift
+++ swiftlabs/main.swift
@@ -15,18 +15,32 @@ import Foundation
 //print(a+b)
 
 //task2
-var numbers = [
-"ноль",
-"один",
-"два",
-"три",
-"четыре",
-"пять",
-"шесть",
-"семь",
-"восемь",
-"девять",
-]
-let a: Int = Int(readLine() ?? "") ?? 0
-let value = numbers[a]
-print(value)
+//var numbers = [
+//"ноль",
+//"один",
+//"два",
+//"три",
+//"четыре",
+//"пять",
+//"шесть",
+//"семь",
+//"восемь",
+//"девять",
+//]
+//let a: Int = Int(readLine() ?? "") ?? 0
+//let value = numbers[a]
+//print(value)
+
+//task3
+var n1 = 0
+var n2 = 1
+var summa = 0
+let n: Int = Int(readLine() ?? "") ?? 0
+var i = 0
+while i < n - 2 {
+    summa = n1 + n2
+    n1 = n2
+    n2 = summa
+    i = i + 1
+}
+print(n2)
